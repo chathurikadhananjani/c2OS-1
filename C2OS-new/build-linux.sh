b60f9c035d4be79313a8a8ebbeb98a9c6f2ba06f
chathurikadhananjani <57212615+chathurikadhananjani@users.noreply.github.com>
2020-07-24 08:27:44 +0530

    Add files via upload

---
+++ C2OS-new/build-linux.sh
@@ -0,0 +1,58 @@
+
+
+if test "`whoami`" != "root" ; then
+	echo "You must be logged in as root to build (for loopback mounting)"
+	echo "Enter 'su' or 'sudo bash' to switch to root"
+	exit
+fi
+
+
+if [ ! -e disk_images/c2os.flp ]
+then
+	echo ">>> Creating new C@OS floppy image..."
+	mkdosfs -C disk_images/c2os.flp 1440 || exit
+fi
+
+
+echo ">>> Assembling bootloader..."
+
+nasm -O0 -w+orphan-labels -f bin -o source/bootload/bootload.bin source/bootload/bootload.asm || exit
+
+
+echo ">>> Assembling C2OS kernel..."
+
+cd source
+nasm -O0 -w+orphan-labels -f bin -o kernel.bin kernel.asm || exit
+cd ..
+
+
+
+echo ">>> Adding bootloader to floppy image..."
+
+dd status=noxfer conv=notrunc if=source/bootload/bootload.bin of=disk_images/mikeos.flp || exit
+
+
+echo ">>> Copying C2OS kernel and programs..."
+
+rm -rf tmp-loop
+
+mkdir tmp-loop && mount -o loop -t vfat disk_images/c2os.flp tmp-loop && cp source/kernel.bin tmp-loop/
+
+cp programs/*.bin programs/*.bas programs/sample.pcx tmp-loop
+
+sleep 0.2
+
+echo ">>> Unmounting loopback floppy..."
+
+umount tmp-loop || exit
+
+rm -rf tmp-loop
+
+
+echo ">>> Creating CD-ROM ISO image..."
+
+rm -f disk_images/c2os.iso
+mkisofs -quiet -V 'C2OS' -input-charset iso8859-1 -o disk_images/c2os.iso -b c2os.flp disk_images/ || exit
+
+echo '>>> Done!'
+
